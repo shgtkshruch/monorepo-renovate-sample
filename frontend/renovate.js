bd49746e9ca9b8aa8326e02e2dc87320f16987fe
shgtkshruch <shgtk.shruch@gmail.com>
2023-01-14 19:29:14 +0900

    Set Git Author

--- frontend/renovate.js
+++ frontend/renovate.js
@@ -10,4 +10,5 @@ module.exports = {
   requireConfig: "optional",
   prConcurrentLimit: process.env.RENOVATE_PR_CONCURRENT_LIMIT,
   repositories: ["shgtkshruch/monorepo-renovate-sample"],
+  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
 };
